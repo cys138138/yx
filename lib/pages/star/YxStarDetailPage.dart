9ce8f9602ba6994c6d64fcdc068537a1562025f8
sun <785400320@qq.com>
2018-12-23 10:57:47 +0800

    艺人详情

--- lib/pages/star/YxStarDetailPage.dart
+++ lib/pages/star/YxStarDetailPage.dart
@@ -2,11 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:yx/utils/WidgetsUtils.dart';
 import 'package:yx/utils/net/YxApi.dart';
 import 'package:yx/utils/net/YxHttp.dart';
-import 'package:flutter_html/flutter_html.dart';
-import 'package:html/dom.dart' as dom;
 
 class YxStarDetailPage extends StatefulWidget {
   String _url, _title, _id;
@@ -44,130 +41,140 @@ class _NewsDetailPageState extends State<YxStarDetailPage> {
     }
 
     return Scaffold(
+      bottomNavigationBar: new Container(
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        child: new Container(
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border(top: BorderSide(color: Colors.grey[100]))),
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              new Expanded(
+                child: new Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: new Row(
+                    children: <Widget>[
+                      new Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: new Column(
+                            children: <Widget>[
+                              new Text(
+                                "预约秒数",
+                                style: new TextStyle(
+                                    fontSize: 11.0,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.grey
+                                ),
+                              ),
+                              new Text(
+                                newsDetail["seconds"].toString() + "秒起",
+                                style: new TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              new Container(
+                height: 50,
+                padding: EdgeInsets.only(left: 15, right: 15),
+                child: FlatButton(
+                  child: Text(
+                    '立即预约',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 252, 130, 45),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: CustomScrollView(
         primary: true,
         slivers: <Widget>[
           SliverAppBar(
             backgroundColor: Colors.transparent,
-            title: new Center(child: new Text("账号或或或"),),
-            expandedHeight: 800.0,
-            flexibleSpace: new SingleChildScrollView(
-              child: new Stack(
-                children: <Widget>[
-                  new Column(
-                    children: <Widget>[
-                      new Container(
-                        height: 300.0,
-                        width: MediaQuery.of(context).size.width,
-                        child: new Image.network(
-                          newsDetail["img_url"],
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      new Container(
-                        height: 60.0,
-                        child: new Padding(
-                          padding: EdgeInsets.all(5.0),
-                          child: new Align(
-                              alignment: Alignment.centerLeft,
-                              child: new Text(
-                                newsDetail["name"],
-                                style: new TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
-                              )),
-                        ),
-                      ),
-                      new Container(
-                        child: new Padding(
-                          padding: EdgeInsets.all(5.0),
-                          child: new Align(
-                              alignment: Alignment.centerLeft,
-                              child: new Text(
-                                newsDetail["short_desc"],
-                                style: new TextStyle(
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.normal,
-                                    color: Colors.black12),
-                              )),
-                        ),
-                      ),
-                      new Container(
-                        child: new Padding(
-                          padding: EdgeInsets.all(5.0),
-                          child: new Align(
-                              alignment: Alignment.centerLeft,
-                              child: new Text(
-                                newsDetail["seconds"].toString() + "秒起",
-                                style: new TextStyle(
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.orange),
-                              )),
-                        ),
-                      ),
-                      new Container(
-                        child: new Padding(
-                          padding: EdgeInsets.all(5.0),
-                          child: new Align(
-                              alignment: Alignment.centerLeft,
-                              child: new Text(
-                                newsDetail["desc"],
-                                style: new TextStyle(
-                                    fontSize: 13.0,
-                                    fontWeight: FontWeight.normal),
-                              )),
-                        ),
-                      ),
-                    ],
-                  ),
-
-
-                  new Positioned(
-                    bottom: 0,
-                    width: MediaQuery.of(context).size.width,
-                    height: 50,
-                    child: new Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border(top: BorderSide(color: Colors.grey[100]))
-                      ),
-                      child: new Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          new Expanded(
-                            child: new Padding(
-                              padding: EdgeInsets.only(left: 8),
-                              child: new Row(
-                                children: <Widget>[
-                                  Text('合计：¥'),
-                                ],
-                              ),
-                            ),
-                          ),
-                          new Container(
-                            height: 50,
-                            padding: EdgeInsets.only(left: 15, right: 15),
-                            child: FlatButton(
-                              child: Text('去结算()', style: TextStyle(color: Colors.white),),
-                              onPressed: (){
-
-                              },
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-
-                ],
+            expandedHeight: 300.0,
+            flexibleSpace: new FlexibleSpaceBar(
+              background: Image.network(
+                newsDetail["img_url"],
+                fit: BoxFit.cover,
               ),
             ),
           ),
+          SliverToBoxAdapter(
+              child: new Container(
+                padding: EdgeInsets.only(left: 10.0),
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  height: 60.0,
+                  child: new Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: new Align(
+                        alignment: Alignment.centerLeft,
+                        child: new Text(
+                          newsDetail["name"],
+                          style: new TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                ),
+                new Container(
+                  child: new Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: new Align(
+                        alignment: Alignment.centerLeft,
+                        child: new Text(
+                          newsDetail["short_desc"],
+                          style: new TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black12),
+                        )),
+                  ),
+                ),
+                new Container(
+                  child: new Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: new Align(
+                        alignment: Alignment.centerLeft,
+                        child: new Text(
+                          newsDetail["seconds"].toString() + "秒起",
+                          style: new TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.orange),
+                        )),
+                  ),
+                ),
+                new Container(
+                  child: new Padding(
+                    padding: EdgeInsets.only(left:10.0,top: 5.0),
+                    child: new Align(
+                        alignment: Alignment.centerLeft,
+                        child: new Text(
+                          newsDetail["desc"],
+                          style: new TextStyle(
+                              fontSize: 13.0, fontWeight: FontWeight.normal),
+                        )),
+                  ),
+                  height: 100.0,
+                ),
+              ],
+            ),
+          )),
         ],
       ),
     );
